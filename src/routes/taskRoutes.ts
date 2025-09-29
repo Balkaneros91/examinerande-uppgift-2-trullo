8005ddbce61469152125188024752ccd50eea341
Antonio-Claudio Andelic <andelic.antonio.claudio91@gmail.com>
2025-09-29 11:32:18 +0200

    Import all controllers instead ctrl*, Add validateBody

--- src/routes/taskRoutes.ts
+++ src/routes/taskRoutes.ts
@@ -1,17 +1,17 @@
 import { Router } from 'express';
-import * as ctrl from '../controllers/taskController.js';
-import { validate } from '../middleware/validate.js';
+import { create, list, get, update, remove } from '../controllers/taskController.js';
+import { validateBody } from '../middleware/validate.js';
 import { createTaskSchema, updateTaskSchema } from '../schemas/task.schema.js';
 
 
 const router = Router();
 
 
-router.post('/', validate(createTaskSchema), ctrl.create);
-router.get('/', ctrl.list);
-router.get('/:id', ctrl.get);
-router.put('/:id', validate(updateTaskSchema), ctrl.update);
-router.delete('/:id', ctrl.remove);
+router.post('/', validateBody(createTaskSchema), create);
+router.get('/', list);
+router.get('/:id', get);
+router.put('/:id', validateBody(updateTaskSchema), update);
+router.delete('/:id', remove);
 
 
 export default router;
--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,17 +1,17 @@
 import { Router } from 'express';
-import * as ctrl from '../controllers/userController.js';
-import { validate } from '../middleware/validate.js';
+import { create, list, get, update, remove } from '../controllers/userController.js';
+import { validateBody } from '../middleware/validate.js';
 import { createUserSchema, updateUserSchema } from '../schemas/user.schema.js';
 
 
 const router = Router();
 
 
-router.post('/', validate(createUserSchema), ctrl.create);
-router.get('/', ctrl.list);
-router.get('/:id', ctrl.get);
-router.put('/:id', validate(updateUserSchema), ctrl.update);
-router.delete('/:id', ctrl.remove);
+router.post('/', validateBody(createUserSchema), create);
+router.get('/', list);
+router.get('/:id', get);
+router.put('/:id', validateBody(updateUserSchema), update);
+router.delete('/:id', remove);
 
 
 export default router;
