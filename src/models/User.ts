161b7ce554a0765043db116e3f613d3f0f1333cc
Antonio-Claudio Andelic <andelic.antonio.claudio91@gmail.com>
2025-09-29 11:26:33 +0200

    Add select: false to password

--- src/models/User.ts
+++ src/models/User.ts
@@ -6,7 +6,7 @@ const UserSchema = new Schema(
 {
 name: { type: String, required: true, trim: true },
 email: { type: String, required: true, unique: true, lowercase: true, trim: true },
-password: { type: String, required: true },
+password: { type: String, required: true, select: false },
 },
 { timestamps: true }
 );
